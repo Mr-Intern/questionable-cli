a1dfc6c0054e34257be0f2c657720d5e28559e3d
mrintern <mrintern@protonmail.com>
2022-08-31 01:00:11 -0400

    try and fail to get get-current-episode executed in main

---
+++ configs/get-current-episode.go
@@ -0,0 +1,8 @@
+package configs
+
+// import "gopkg.in/yaml.v3"
+import "fmt"
+
+func getCurrentEpisode() {
+	fmt.Println("get-current-episode called")
+}	
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
 import "github/Mr-Intern/questionable-cli/cmd"
+import "github/Mr-Intern/questionable-cli/configs"
 
 func main() {
 	cmd.Execute()
+	configs.GetCurrentEpisode()
 }
