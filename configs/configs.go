beee00eddba6c45d0885d3d21ac430fed18a4131
mrintern <mrintern@protonmail.com>
2022-09-04 20:22:48 -0400

    read from yaml file almost working

--- configs/configs.go
+++ configs/configs.go
@@ -1,8 +1,30 @@
 package configs
 
-// import "gopkg.in/yaml.v3"
-import "fmt"
+import ( 
+	"fmt"
+	"io/ioutil"
+	"gopkg.in/yaml.v3"
+	"log"
+)
 
 func GetCurrentEpisode() {
-	fmt.Println("get-current-episode called")
+	fmt.Println("GetCurrentEpisode called")
+	
+	// https://zetcode.com/golang/yaml/
+	yfile, err := ioutil.ReadFile("configs.yaml")
+	
+	if err != nil {
+          log.Fatal(err)
+  	}
+
+
+	data := make(map[interface{}]interface{})
+
+	yaml.Unmarshal(yfile, &data)
+
+	for k, v := range data {
+		fmt.Printf("%s -> %d\n", k, v)
+	}
+
+
 }	
